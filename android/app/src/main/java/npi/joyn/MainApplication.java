56c2325e9a1ceccc36f86e29736440d46ef25149
Zarathon Maia <zarathonmaia@gmail.com>
2017-10-12 10:16:57 -0300

    Add facebook logic and retrieve some data

---
+++ android/app/src/main/java/npi/joyn/MainApplication.java
@@ -0,0 +1,61 @@
+package npi.joyn;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import npi.joyn.BuildConfig;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
+   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+     @Override
+     public boolean getUseDeveloperSupport() {
+       return BuildConfig.DEBUG;
+     }
+
+     @Override
+     protected List<ReactPackage> getPackages() {
+       return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                new FBSDKPackage(mCallbackManager),
+                new VectorIconsPackage(),
+                new RCTCameraPackage(),
+                new RCTSplashScreenPackage()    //register Module
+       );
+     }
+   };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    FacebookSdk.sdkInitialize(getApplicationContext());
+    // If you want to use AppEventsLogger to log events.
+    AppEventsLogger.activateApp(this);
+  }
+}
